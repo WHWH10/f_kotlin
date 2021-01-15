089ae45816808ecc8bac7946128f6c70bcd08845
WHWH10 <eyet010@gmail.com>
2021-01-15 17:30:54 +0900

    Android UI & PNG

---
+++ app/src/main/java/com/example/myapplication/안드로이드_UI/01.How_to_draw_ui.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.안드로이드_UI
+
+//210115
+// 01. 안드로이드에서 화면을 그리는 방법
+
+// - XML 이용한다
+// - DSL Language : Domain Specific Language
+// - 안드로이드 UI 그리기 위해 특화된 언어
+
+// 핸드폰마다 화면 크기가 다 다른데 어떻게 그려야 할까
+// - 픽셀, dpi, dp
+// - 픽셀 : 핸드폰 화면에서 빛이 나오는 전구 -> 가장 작은 단위
+// - dpi : dot per inch
+//   - ldpi : 120 (1인치에 120픽셀)
+//   - mdpi : 160 (1인치에 240픽셀)
+//   - hdpi : 240
+//   - xhdpi : 320
+//   - xxhdpi : 480
+//   - xxxhdpi : 640
+// - dp : Density Independent Pixel
+//   - 픽셀 독립적인 단위 ( 핸드폰 해상도 상관없이 동일하게 나옴)
---
+++ app/src/main/java/com/example/myapplication/안드로이드_UI/02.ViewComponent.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.안드로이드_UI
+
+//210115
+// 02. ViewComponent
+
+// - 화면 그리는 요소
+// ex) Button. Text. .. 등
